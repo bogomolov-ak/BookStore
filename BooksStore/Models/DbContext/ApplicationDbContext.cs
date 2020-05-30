1b1785d78f5711cd9eac1b714dab4c7bc1991846
bogomolov-ak <59489495+bogomolov-ak@users.noreply.github.com>
2020-05-30 22:20:56 +0300

    Add files via upload

---
+++ BooksStore/Models/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Models.Classes;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Models.DbContext
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ BooksStore/Models/ViewModels/Identity/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models.ViewModels.Identity
+{
+    public class LoginViewModel
+    {
+        [Required] 
+        public string Name { get; set; }
+
+        [Required] 
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
